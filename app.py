0dcf546a23eff454c41d07d2ede5001e45ec7058
elvinsavio <elvinsavio.lp@gmail.com>
2025-10-30 17:57:46 +0000

    re-setup

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
 from flask import Flask, render_template
 import os
 
-template_dir = os.path.abspath("src/templates")
-static_dir = os.path.abspath("src/static")
+template_dir = os.path.abspath("templates")
+static_dir = os.path.abspath("static")
 
 app = Flask(
     __name__, template_folder=template_dir, static_url_path="", static_folder=static_dir
